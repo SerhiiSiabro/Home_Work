89deed306c994a824df2cba3ee743241d4d43ed7
Сергій Сябро <serhiisyabro@Sergijs-MacBook-Pro.local>
2023-03-07 21:16:16 +0300

    switch/case - changes with understanding

--- HW-4/script.js
+++ HW-4/script.js
@@ -1,57 +1,57 @@
 const operation = prompt('Hello!\nChoose operator (+, -, * , /, cos, sin, pow)!')
-
-const firstNumber =  Number(prompt('Enter, please the first number'));
-const secondNumber = Number(prompt('Enter, please the second number'));
+let fullNameOfOperation;
+let result;
 
 switch (operation) {
-    case '+': {
-        firstNumber;
-        secondNumber;
-        const resultSum = firstNumber + secondNumber;
-        alert ('Sum: ' + firstNumber + ' + ' + secondNumber + ' = ' + resultSum);
-        break; 
-    }
-    case '-': {
-        firstNumber;
-        secondNumber;
-        const resultDiff = firstNumber - secondNumber;
-        alert ('Diff: ' + firstNumber + ' - ' + secondNumber + ' = ' + resultDiff);
-        break; 
-    }
-    case '*': {
-        firstNumber;
-        secondNumber;
-        const resultMult = firstNumber * secondNumber;
-        alert ('Mult: ' + firstNumber + ' * ' + secondNumber + ' = ' + resultMult);
-        break; 
-    }
-    case '/': {
-        firstNumber;
-        secondNumber;
-        const resultDiv = firstNumber / secondNumber;
-        alert ('Div: ' + firstNumber + ' / ' + secondNumber + ' = ' + resultDiv);
-        break; 
-    }
-    case 'cos': {
-        const number = Number(prompt('Enter, please the number'));
-        const cosNumber = Math.cos(number);
-        alert ('Cos(' + number + ') =' + cosNumber);
-        break; 
-    }
-    case 'sin': {
-        const number = Number(prompt('Enter, please the number'));
-        const sinNumber = Math.sin(number);
-        alert ('Sin(' + number + ') =' + sinNumber);
-        break; 
+    case '+': case '-': case '*': case '/': {
+        const firstNumber =  Number(prompt('Enter, please the first number'));
+        const secondNumber = Number(prompt('Enter, please the second number'));
+        switch (operation) {
+            case '+': {
+                fullNameOfOperation = 'Sum: ';
+                result = firstNumber + secondNumber;
+                break;
+            }
+            case '-': {
+                fullNameOfOperation = 'Diff: ';
+                result = firstNumber - secondNumber;
+                break;
+            }
+            case '*': {
+                fullNameOfOperation = 'Mult: ';
+                result = firstNumber * secondNumber;
+                break;
+            }
+            case '/': {
+                fullNameOfOperation = 'Div: ';
+                result = firstNumber / secondNumber;
+                break;
+            }
+        }
+        alert (`${fullNameOfOperation + firstNumber} ${operation} ${secondNumber} = ${result}`);
+        break;
     }
     case 'pow': {
-        const firstNumberPow = Number(prompt('Enter, please the base number'));
-        const secondNumberPow = Number(prompt('Enter, please the exponent number'));
-        const resultPow = Math.pow(firstNumberPow, secondNumberPow);
-        alert ('Power(' + firstNumber + ',' + secondNumber +') =' + resultPow);
-        break; 
+        firstNumber = Number(prompt('Enter, please the base number'));
+        secondNumber = Number(prompt('Enter, please the exponent number'));
+        result = Math.pow(firstNumber, secondNumber);
+        alert ('Power(' + firstNumber + ',' + secondNumber +') =' + result);
+        break;
     }
-    default:
-        alert ("This is an invalid operator!");
-    
+    case 'sin': case 'cos': {
+        const number = Number(prompt('Enter, please the number'));
+        switch (operation) {
+            case 'sin': {
+                fullNameOfOperation = 'sin(';
+                result = Math.sin(number);
+                break;
+            }
+            case 'cos': {
+                fullNameOfOperation = 'cos(';
+                result = Math.cos(number);
+                break;
+            }
+        }
+        alert (`${fullNameOfOperation + result})`)
+    }
 }
